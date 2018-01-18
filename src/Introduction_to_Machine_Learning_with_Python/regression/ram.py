3384936130a9ad440250061eb28dc06aeaa30a35
vladimir borovic <vladimir.borovic@gmail.com>
2018-01-18 09:35:20 +0200

    refactoring ram

--- src/Introduction_to_Machine_Learning_with_Python/regression/ram.py
+++ src/Introduction_to_Machine_Learning_with_Python/regression/ram.py
@@ -6,39 +6,54 @@ from sklearn.ensemble.gradient_boosting import GradientBoostingRegressor
 from sklearn.linear_model.base import LinearRegression
 from sklearn.tree import DecisionTreeRegressor
 
-ram_prices = pd.read_csv("ram_price.csv", index_col=0)
-fig, axes = plt.subplots(1, 2, figsize=(10, 3))
-axes[0].semilogy(ram_prices.date, ram_prices.price)
-axes[0].set_xlabel("Год")
-axes[0].set_ylabel("Цена $/Мбайт")
-plt.semilogy(ram_prices.date, ram_prices.price)
 
-# используем исторические данные для прогнозирования цен после 2000 года
-data_train = ram_prices[ram_prices.date < 2000]
-plt.semilogy(data_train.date, data_train.price, label="Обучающие данные")
+def predict_for_methods(ram_prices, *methods):
+    # используем исторические данные для прогнозирования цен после 2000 года
+    data_train = ram_prices[ram_prices.date < 2000]
+    # прогнозируем цены по датам
+    X_train = data_train.date[:, np.newaxis]
+    # мы используем логпреобразование, что получить простую взаимосвязь между данными и откликом
+    y_train = np.log(data_train.price)
 
-# прогнозируем цены по датам
-X_train = data_train.date[:, np.newaxis]
-# мы используем логпреобразование, что получить простую взаимосвязь между данными и откликом
-y_train = np.log(data_train.price)
-# прогнозируем по всем данным
-X_all = ram_prices.date[:, np.newaxis]
+    # прогнозируем по всем данным
+    X_all = ram_prices.date[:, np.newaxis]
 
-methods = [
-    LinearRegression,
-    DecisionTreeRegressor,
-    (GradientBoostingRegressor, dict(random_state=0, max_depth=1)),
-    (RandomForestRegressor, dict(n_estimators=500, random_state=2))
-]
-for method in methods:
-    kwargs = dict()
-    if isinstance(method, tuple):
-        method, kwargs = method
-    reg = method(**kwargs).fit(X_train, y_train)
-    # экспоненцируем, чтобы обратить логарифмическое преобразование
-    predict = reg.predict(X_all)
-    predict_price = np.exp(predict)
-    plt.semilogy(ram_prices.date, predict_price, label=f'Прогнозы {method.__name__}')
+    result = {}
+    for method in methods:
+        kwargs = {}
+        if isinstance(method, tuple):
+            method, kwargs = method
+        prediction = method(**kwargs).fit(X_train, y_train).predict(X_all)
+        # экспоненцируем, чтобы обратить логарифмическое преобразование
+        result[method.__name__] = np.exp(prediction)
+    return result
 
-plt.legend()
-plt.show()
+
+def semilogy(ax, date, price, label):
+    ax.semilogy(date, price, label=label)
+    ax.set_xlabel("Год")
+    ax.set_ylabel("Цена $/Мбайт")
+
+
+def draw_plot(ram_prices, *methods):
+    fig, axes = plt.subplots(1, 2, figsize=(10, 3))
+    semilogy(axes[0], ram_prices.date, ram_prices.price, label="Оригинальные данные")
+    semilogy(axes[1], ram_prices.date, ram_prices.price, label="Обучающие данные")
+
+    predictions = predict_for_methods(ram_prices, *methods)
+    for method, predict_price in predictions.items():
+        semilogy(axes[1], ram_prices.date, predict_price, label=f'Прогнозы {method}')
+
+    plt.legend()
+    plt.show()
+
+
+if __name__ == '__main__':
+    ram_prices = pd.read_csv("ram_price.csv", index_col=0)
+    draw_plot(
+        ram_prices,
+        LinearRegression,
+        DecisionTreeRegressor,
+        (GradientBoostingRegressor, dict(random_state=0, max_depth=1)),
+        (RandomForestRegressor, dict(n_estimators=500, random_state=2))
+    )
