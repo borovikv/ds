fe86a44f3aac85e11674af9aa3f8c2b1164d3ae4
vladimir borovic <vladimir.borovic@ellation.com>
2019-04-11 10:28:31 +0300

    linear algebra

---
+++ src/DS_from_scratch_Joel_Grus/linear_algebra.py
@@ -0,0 +1,65 @@
+import math
+from functools import reduce
+
+from clint.textui import cols
+
+
+def vector_add(v, w):
+    return [v_i + w_i for v_i, w_i in zip(v, w)]
+
+
+def vector_subtract(v, w):
+    return [v_i - w_i for v_i, w_i in zip(v, w)]
+
+
+def scalar_multiply(c, v):
+    return [c * v_i for v_i in v]
+
+
+def vector_sum(vs):
+    return reduce(vector_add, vs)
+
+
+def vector_mean(vectors):
+    n = len(vectors)
+    scalar_multiply(1 / n, vector_sum(vectors))
+
+
+def dot(v, w):
+    # https://ru.wikipedia.org/wiki/Скалярное_произведение#Геометрическое_определение
+    return sum(v_i * w_i for v_i, w_i in zip(v, w))
+
+
+def sum_of_squares(v):
+    return dot(v, v)
+
+
+def magnitude(v):
+    return math.sqrt(sum_of_squares(v))
+
+
+def squared_distance(v, w):
+    """ :return (v1 - w1)**2 + ... + (vi - wi)**2 """
+    return sum_of_squares(vector_subtract(v, w))
+
+
+# def distance(v, w):
+#     return math.sqrt(squared_distance(v, w))
+
+
+def distance(v, w):
+    return magnitude(vector_subtract(v, w))
+
+
+# Matrix
+
+def shape(A):
+    return len(A), len(A[0]) if A else 0
+
+
+def column(A, j):
+    return [row[j] for row in A]
+
+
+def matrix(rows: int, cols: int, entry_fn: (int, int)):
+    return [[entry_fn(i, j) for j in range(cols)] for i in range(rows)]
