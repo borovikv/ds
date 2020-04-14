a5b2963461c60a6187c15db7f6bef1b441b131a4
vladimir borovic <vladimir.borovic@ellation.com>
2020-04-14 14:14:02 +0300

    t4.3

--- src/preparation/graphs/tasks.py
+++ src/preparation/graphs/tasks.py
@@ -1,4 +1,5 @@
 from collections import deque
+import timeit
 
 
 def find_path_breadth(graph, start, end):
@@ -53,6 +54,9 @@ class Node:
         self.left = None
         self.right = None
 
+    def __repr__(self):
+        return str(self.value)
+
     def pre_order_traversal(self, indent=0):
         print('\t' * indent + f'{self.value}')
         indent += 1
@@ -71,5 +75,38 @@ def create_binary_tree(a):
     return n
 
 
-n = create_binary_tree(list(range(1, 8)))
+def create_binary_tree_2(a, start=0, stop=None):
+    if stop is None:
+        stop = len(a) - 1
+    if stop < start:
+        return
+    middle = (start + stop) // 2
+    n = Node(a[middle])
+    n.left = create_binary_tree_2(a, start, middle - 1)
+    n.right = create_binary_tree_2(a, middle + 1, stop)
+    return n
+
+
+n = create_binary_tree_2(list(range(1, 800)))
 n.pre_order_traversal()
+
+
+# 4.3
+def get_level_nodes(root, level):
+    if level == 0:
+        return [root]
+    result = []
+    for n in get_level_nodes(root, level - 1):
+        result += [n.left, n.right]
+    return result
+
+
+print(get_level_nodes(n, 0))
+print(get_level_nodes(n, 1))
+print(get_level_nodes(n, 2))
+
+
+t = timeit.timeit('create_binary_tree(list(range(1, 8000)))', "from __main__ import create_binary_tree", number=10)
+print(t)
+t = timeit.timeit('create_binary_tree_2(list(range(1, 8000)))', "from __main__ import create_binary_tree_2", number=10)
+print(t)
