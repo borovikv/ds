7fd1cac8696a75c0951f5197c51f761ae5ca8e5e
vladimir borovic <vladimir.borovic@ellation.com>
2020-04-29 11:35:13 +0300

    dj

--- src/amz/roots_of_equision.py
+++ src/amz/roots_of_equision.py
@@ -6,13 +6,14 @@ for a in range(n):
         for c in range(n):
             for d in range(n):
                 if a ** 3 + b ** 3 == c ** 3 + d ** 3:
-                    r.append(tuple({a, b, c, d}))
+                    r.append((a, b, c, d))
 # print(set(r))
 print(len(set(r)))
+print(r)
+x = [[(a, b, a, b), (b, a, b, a)] if a != b else [(a, b, a, b)] for a in range(n) for b in range(n)]
+print(x)
 
-x = [(a, b) for a in range(n) for b in range(n)]
-
-print(len(set(x)))
+# print(len(set(x)))
 print(len(r), len(x))
 
 
